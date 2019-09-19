e3dcd0df40b8b4bda15345e8d336891e5fafbadc
hrupesh <rcashok0@gmail.com>
2019-09-19 20:08:41 +0530

    mmmmm

--- src/app/components/caledarcomponent/caledarcomponent.component.ts
+++ src/app/components/caledarcomponent/caledarcomponent.component.ts
@@ -14,7 +14,7 @@ export class CaledarcomponentComponent implements OnInit {
   stock = [];
 
   payload = {
-    month:"7",
+    month:7,
     year:2019
   }
 
@@ -36,6 +36,54 @@ export class CaledarcomponentComponent implements OnInit {
   }
 
   onSubmit(){
+    document.getElementById('contents').style.visibility="hidden";
+    document.getElementById('load').style.visibility="visible";
+    this.calendarS.getC(this.payload).subscribe(data => {
+      this.data = data;
+      this.plant_details = data.success.data.plant_details;
+      this.stock = data.success.data.stock;
+      console.log(this.data);
+      console.log(data);
+      console.log(this.plant_details);
+      console.log(this.stock);
+      document.getElementById('load').style.visibility="hidden";
+      document.getElementById('contents').style.visibility="visible";
+    });
+  }
+
+  onPrev(){
+    if(this.payload.month > 0){
+      this.payload.month--;
+    }
+    if(this.payload.month == 0){
+      this.payload.year--;
+      this.payload.month = 12;
+    }
+    document.getElementById('contents').style.visibility="hidden";
+    document.getElementById('load').style.visibility="visible";
+    this.calendarS.getC(this.payload).subscribe(data => {
+      this.data = data;
+      this.plant_details = data.success.data.plant_details;
+      this.stock = data.success.data.stock;
+      console.log(this.data);
+      console.log(data);
+      console.log(this.plant_details);
+      console.log(this.stock);
+      document.getElementById('load').style.visibility="hidden";
+      document.getElementById('contents').style.visibility="visible";
+    });
+  }
+
+  onNext(){
+    if(this.payload.month < 13){
+      this.payload.month++;
+    }
+    if(this.payload.month == 13){
+      this.payload.year++;
+      this.payload.month = 1;
+    }
+    document.getElementById('contents').style.visibility="hidden";
+    document.getElementById('load').style.visibility="visible";
     this.calendarS.getC(this.payload).subscribe(data => {
       this.data = data;
       this.plant_details = data.success.data.plant_details;
